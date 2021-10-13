837f38af934fdb8a163f64716f454ae48d369349
myoga <34601860+hsytkm@users.noreply.github.com>
2021-10-13 22:07:02 +0900

    プロジェクト ファイルを追加します。

---
+++ SettingsSettingsGenerator/AttributeFieldSource.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace SettingsSettingsGenerator
+{
+    internal class AttributeFieldSource
+    {
+        internal enum FieldDeclarationType { BuiltIn, String, /*Enum*/ }
+
+        internal string FieldName { get; }
+        internal string TypeName { get; }
+        internal FieldDeclarationType FieldType { get; }
+        internal string Key { get; }
+
+        private AttributeFieldSource(string name, string type, FieldDeclarationType decl, string key)
+            => (FieldName, TypeName, FieldType, Key) = (name, type, decl, key);
+
+        internal static AttributeFieldSource? Create(SemanticModel semanticModel, FieldDeclarationSyntax fieldDeclaration)
+        {
+            var fieldSymbol = fieldDeclaration.Declaration.Variables.Select(v => semanticModel.GetDeclaredSymbol(v) as IFieldSymbol).FirstOrDefault();
+            if (fieldSymbol is null)
+                return null;
+
+            var typeName = fieldSymbol.Type.ToString(); //.Split('.').Last();
+            var fieldDecl = typeName.Contains("string") ? FieldDeclarationType.String : FieldDeclarationType.BuiltIn;
+
+            // target is only Nullable type.
+            if (typeName[typeName.Length - 1] != '?')
+                return null;
+
+            var key = GetKeyName(fieldDeclaration);
+            if (key is null)
+                return null;
+
+            return new(fieldSymbol.Name, typeName, fieldDecl, key);
+        }
+
+        private static string? GetKeyName(FieldDeclarationSyntax fieldDeclaration)
+        {
+            var attr = fieldDeclaration.AttributeLists.SelectMany(x => x.Attributes)
+                .FirstOrDefault(x => x.Name.ToString() is nameof(SettingsGenerator) or SettingsGenerator.AttributeName);
+
+            IReadOnlyList<AttributeArgumentSyntax>? args = attr?.ArgumentList?.Arguments;
+            if (args is not null && args.Count >= 1)
+            {
+                var key = args.Take(2).Select(x => x.ToString()).FirstOrDefault() ?? "";
+                return key;
+            }
+            return null;
+        }
+
+    }
+}
---
+++ SettingsSettingsGenerator/SourceGenerator.cs
@@ -0,0 +1,85 @@
+﻿#pragma warning disable CA1031
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SettingsSettingsGenerator
+{
+    [Generator]
+    public sealed class SettingsGenerator : ISourceGenerator
+    {
+        internal const string AttributeName = nameof(SettingsGenerator) + "Attribute";
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+#if DEBUG
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                //System.Diagnostics.Debugger.Launch();
+            }
+#endif
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var attrCode = new SettingsGeneratorAttributeTemplate().TransformText();
+            context.AddSource(AttributeName + ".cs", attrCode);
+
+            try
+            {
+                if (context.SyntaxReceiver is not SyntaxReceiver receiver) return;
+
+                foreach (var classDeclaration in receiver.Targets)
+                {
+                    var model = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
+                    var typeSymbol = model.GetDeclaredSymbol(classDeclaration);
+                    if (typeSymbol is null) continue;
+
+                    var candidateFields = classDeclaration.Members.OfType<FieldDeclarationSyntax>()
+                        .Select(field => (field, model))
+                        .SelectMany(x => x.field.Declaration.Variables.Select(v => (x.field, symbol: x.model.GetDeclaredSymbol(v) as IFieldSymbol)))
+                        .Where(x => x.symbol?.GetAttributes().Any(y => y.AttributeClass?.Name is nameof(SettingsGenerator) or AttributeName) ?? false)
+                        .ToArray();
+
+                    var fieldSource = candidateFields.Select(x => AttributeFieldSource.Create(model, x.field)).ToArray();
+                    if (fieldSource is null) continue;
+
+                    var template = new CodeTemplate(classDeclaration, fieldSource!)
+                    {
+                        Namespace = typeSymbol.ContainingNamespace.ToDisplayString(),
+                    };
+
+                    var text = template.TransformText();
+                    context.AddSource(typeSymbol.GenerateHintName(), text);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.ToString());
+            }
+        }
+
+        private sealed class SyntaxReceiver : ISyntaxReceiver
+        {
+            internal List<ClassDeclarationSyntax> Targets { get; } = new();
+
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+                if (syntaxNode is not ClassDeclarationSyntax classDeclaration) return;
+
+                var fields = classDeclaration.Members.OfType<FieldDeclarationSyntax>();
+                if (fields is null) return;
+
+                var attr = fields.SelectMany(x => x.AttributeLists)?.SelectMany(x => x.Attributes)
+                    .FirstOrDefault(x => x.Name.ToString() is nameof(SettingsGenerator) or AttributeName);
+                if (attr is null) return;
+
+                Targets.Add(classDeclaration);
+            }
+        }
+    }
+}
